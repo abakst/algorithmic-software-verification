c064622a13ed88af851974bbf0897608db473b30
Alexander Bakst <abakst@cs.ucsd.edu>
2013-05-11 15:17:46 -0700

    locks-fun

--- assignments/hw1-nanojs/tests/pos/locks-fun.js
+++ assignments/hw1-nanojs/tests/pos/locks-fun.js
@@ -11,15 +11,20 @@ function pos(){
 }
 
 function create(){
+  ensures($result == 0);
   return 0;
 }
 
 function acquire(l){
+  requires(l == 0);
+  ensures($result == 1);
   assert(l==0);
   return 1;
 }
 
 function release(l){
+  requires(l == 1);
+  ensures($result == 0);
   assert(l==1);
   return 0;
 }
@@ -30,6 +35,7 @@ function main(){
   var l    = create();
 
   while (0 < n){
+    invariant(l == 0);
     if (0 < flag){ 
       l = acquire(l); 
     }
