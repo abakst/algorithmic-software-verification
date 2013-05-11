6e91f5784301b4f22e95402ca2f7290ca8999c59
Alexander Bakst <abakst@cs.ucsd.edu>
2013-05-11 15:18:29 -0700

    locks-loop-flat

--- assignments/hw1-nanojs/tests/pos/locks-loop-flat.js
+++ assignments/hw1-nanojs/tests/pos/locks-loop-flat.js
@@ -1,6 +1,7 @@
 function locks(n, flag){
   var lock = 0;
   while (n > 0){
+    invariant(lock == 0);
     if (0 < flag){ lock = 1 };
     if (0 < flag){ lock = 0 };
     if (0 < flag){ lock = 1 };
